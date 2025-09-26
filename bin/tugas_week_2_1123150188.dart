43c907dc6c0aae4756b6888fb1dd62b4be562fa6
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2025-09-26 20:10:57 +0700

    first commit

---
+++ bin/tugas_week_2_1123150188.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_week_2_1123150188/tugas_week_2_1123150188.dart' as tugas_week_2_1123150188;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_week_2_1123150188.calculate()}!');
+}
